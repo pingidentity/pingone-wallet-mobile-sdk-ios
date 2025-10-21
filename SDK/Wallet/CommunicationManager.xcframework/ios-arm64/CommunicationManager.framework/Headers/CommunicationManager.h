b8006bd5fd99c41738deb7fdf041dbc8c2d2230a
kdevipriya55 <kdevipriya@pingidentity.com>
2025-10-21 22:58:50 +0530

    Updated frameworks for SDHv2.0.3

---
+++ SDK/Wallet/CommunicationManager.xcframework/ios-arm64/CommunicationManager.framework/Headers/CommunicationManager.h
@@ -0,0 +1,18 @@
+//
+//  CommunicationManager.h
+//  CommunicationManager
+//
+//  Created by Bhavya Chauhan on 3/17/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CommunicationManager.
+FOUNDATION_EXPORT double CommunicationManagerVersionNumber;
+
+//! Project version string for CommunicationManager.
+FOUNDATION_EXPORT const unsigned char CommunicationManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommunicationManager/PublicHeader.h>
+
+
---
+++ SDK/Wallet/PingOneWallet.xcframework/ios-arm64_x86_64-simulator/PingOneWallet.framework/Headers/PingOneWallet.h
@@ -0,0 +1,18 @@
+//
+//  PingOneWallet.h
+//  PingOneWallet
+//
+//  Created by Bhavya Chauhan on 3/17/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PingOneWallet.
+FOUNDATION_EXPORT double PingOneWalletVersionNumber;
+
+//! Project version string for PingOneWallet.
+FOUNDATION_EXPORT const unsigned char PingOneWalletVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PingOneWallet/PublicHeader.h>
+
+
